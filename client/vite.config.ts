6645e405bba7b672cfe96b48417aea0681569759
Dayan Perez <dayan.perez9012@gmail.com>
2024-04-14 16:26:59 -0300

    project setup with docker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/",
+  publicDir: "public",
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,21 @@
-import app from "./app";
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
-// Start the server
-const PORT: number = 3305;
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: is running on http://localhost:${PORT}`);
+// Load environment variables
+dotenv.config();
+
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: is running on http://localhost:${port}`);
 });
