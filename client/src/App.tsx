a7301c67b0f6621f84b6a394b022613e0d5cb137
Dayan Perez <dayan.perez9012@gmail.com>
2024-04-16 14:59:44 -0300

    shadcn-ui setup (#5)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button>Loggin</Button>
     </>
   );
 }
