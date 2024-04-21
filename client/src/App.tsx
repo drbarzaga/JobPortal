cdf3ad54026856104d9d5b3db669a593dcb0f45f
Dayan Perez <dayan.perez9012@gmail.com>
2024-04-21 14:10:14 -0300

    adjusting ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import {
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import LocationSelect from "./components/LocationSelect";
+// import LocationSelect from "./components/LocationSelect";
 
 const navigation = [
   { name: "Home", href: "#", icon: HomeIcon },
